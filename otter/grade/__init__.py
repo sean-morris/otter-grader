0ffe22ff56db4ad6253cc2cd25cbf8153feba931
sean-morris <sean.smorris@gmail.com>
2024-07-16 14:36:44 -0700

    Otter Grade Write Individual Notebook Results

--- otter/grade/__init__.py
+++ otter/grade/__init__.py
@@ -10,6 +10,7 @@ from .containers import launch_containers
 from .utils import (
     merge_csv,
     prune_images,
+    POINTS_POSSIBLE_LABEL,
     SCORES_DICT_FILE_KEY,
     SCORES_DICT_PERCENT_CORRECT_KEY,
     SCORES_DICT_TOTAL_POINTS_KEY,
@@ -153,6 +154,16 @@ def main(
 
     # write to CSV file
     output_df.to_csv(os.path.join(output_dir, "final_grades.csv"), na_rep='--', index=False)
+
+    # write score summaries to files
+    grading_summary_path = os.path.join(output_dir, "grading-summaries")
+    if not os.path.exists(grading_summary_path):
+        os.mkdir(grading_summary_path)
+    for df in grade_dfs:
+        df_dict = df.to_dict()
+        if df_dict['file'][0] != POINTS_POSSIBLE_LABEL:
+            with open(os.path.join(grading_summary_path, f"{df_dict['file'][0]}.txt"), mode="w") as f:
+                f.write(df_dict["summary"][0])
 
     # return percentage if a single file was graded
     if len(paths) == 1 and os.path.isfile(paths[0]):
